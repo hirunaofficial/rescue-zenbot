9c16a0bd53d3dde151d65ca94507c9396403c9c8
GD Hiruna <hirunaofficial@gmail.com>
2024-12-06 07:30:08 +0530

    Updated viewport meta tag

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="assets/css/chatbot.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <div class="chat-container">
